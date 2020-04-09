827b3657b03f1631dc27786f23cd315d5ee47635
lucaspontoexe <ltx16@hotmail.com>
2020-04-09 17:49:56 -0300

    Updates

---
+++ precache-manifest.f3ed1e11e85d7469080c8a5aab4a1620.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb9a802e46fc680da2905b9abb5063d",
+    "url": "/lucaspontoexe/pwa-test/index.html"
+  },
+  {
+    "revision": "0fe9ed57d0e793990cde",
+    "url": "/lucaspontoexe/pwa-test/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8ecfd8f47b4c7a647a72",
+    "url": "/lucaspontoexe/pwa-test/static/js/2.58d298bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lucaspontoexe/pwa-test/static/js/2.58d298bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0fe9ed57d0e793990cde",
+    "url": "/lucaspontoexe/pwa-test/static/js/main.2d964309.chunk.js"
+  },
+  {
+    "revision": "2693d0d549de224291a2",
+    "url": "/lucaspontoexe/pwa-test/static/js/runtime-main.e29674c5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/lucaspontoexe/pwa-test/static/media/logo.5d5d9eef.svg"
+  }
+]);
