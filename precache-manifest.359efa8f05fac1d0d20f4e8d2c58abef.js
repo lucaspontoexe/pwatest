1f9c0007ef93b4eb9dc16b81db14cff956379b00
lucaspontoexe <ltx16@hotmail.com>
2020-04-18 10:27:48 -0300

    Updates

--- precache-manifest.359efa8f05fac1d0d20f4e8d2c58abef.js
+++ precache-manifest.359efa8f05fac1d0d20f4e8d2c58abef.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "10357aa885cea058ab61a154446f127a",
+    "revision": "e7e1f357d90dc7cb2dbdb60d8cd568bc",
     "url": "/pwatest/index.html"
   },
   {
-    "revision": "45c695614a3ac5b03754",
+    "revision": "4ceda0a9f67c8103df85",
     "url": "/pwatest/static/css/main.5f361e03.chunk.css"
   },
   {
-    "revision": "8ecfd8f47b4c7a647a72",
-    "url": "/pwatest/static/js/2.58d298bd.chunk.js"
+    "revision": "e882f1643a5c6e1f182a",
+    "url": "/pwatest/static/js/2.9191c359.chunk.js"
   },
   {
     "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
-    "url": "/pwatest/static/js/2.58d298bd.chunk.js.LICENSE.txt"
+    "url": "/pwatest/static/js/2.9191c359.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "45c695614a3ac5b03754",
-    "url": "/pwatest/static/js/main.40769996.chunk.js"
+    "revision": "4ceda0a9f67c8103df85",
+    "url": "/pwatest/static/js/main.736f46a2.chunk.js"
   },
   {
     "revision": "20acb566ab92871a8857",
--- service-worker.js
+++ service-worker.js
@@ -14,13 +14,31 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/pwatest/precache-manifest.6fc2ad21b8074e507725290d0dcc38a3.js"
+  "/pwatest/precache-manifest.359efa8f05fac1d0d20f4e8d2c58abef.js"
 );
 
 self.addEventListener('message', (event) => {
   if (event.data && event.data.type === 'SKIP_WAITING') {
     self.skipWaiting();
   }
+});
+
+self.addEventListener('fetch', event => {
+  // ignore all requests with are not of method POST and which are not the URL we defined in in share_target as action
+  if (event.request.method !== 'POST') {
+      return;
+  }
+
+  // Code mostly from https://paul.kinlan.me/file-web-share-target/
+  event.respondWith(Response.redirect('https://lucaspontoexe.github.io/pwatest/'));
+  event.waitUntil(async function() {
+      const data = await event.request.formData();
+      const client = await self.clients.get(event.resultingClientId || event.clientId);
+
+      const file = data.get('file');
+      // send the image data to the client
+      client.postMessage({ file, action: 'load-image' });
+  }());
 });
 
 workbox.core.clientsClaim();
